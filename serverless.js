7063f6506ffa21f904be47436e6f724d70f8bc70
Eslam A. Hefnawy <eahefnawy@gmail.com>
2019-07-15 20:13:03 +0300

    updated core

--- serverless.js
+++ serverless.js
@@ -1,6 +1,6 @@
 const aws = require('aws-sdk')
 const { pick, mergeDeepRight, not } = require('ramda')
-const { Component } = require('@serverless/components')
+const { Component } = require('@serverless/core')
 const {
   getApiId,
   createApi,
@@ -14,19 +14,19 @@ const {
   getWebsocketUrl
 } = require('./utils')
 
-const outputMask = [
+const outputsList = [
   'name',
-  'deploymentStage',
+  'stage',
   'description',
   'routeSelectionExpression',
   'routes',
   'id',
-  'url'
+  'url',
+  'region'
 ]
 
 const defaults = {
-  name: 'serverless',
-  deploymentStage: 'dev',
+  stage: 'dev',
   description: 'Serverless WebSockets',
   routeSelectionExpression: '$request.body.action',
   routes: {}, // key (route): value (lambda arn)
@@ -36,6 +36,9 @@ const defaults = {
 class WebSockets extends Component {
   async default(inputs = {}) {
     const config = mergeDeepRight(defaults, inputs)
+
+    config.name = this.state.name || this.context.resourceId()
+
     const apig2 = new aws.ApiGatewayV2({
       region: config.region,
       credentials: this.context.credentials.aws
@@ -45,7 +48,10 @@ class WebSockets extends Component {
       credentials: this.context.credentials.aws
     })
 
-    this.ui.status(`Deploying`)
+    this.context.status(`Deploying`)
+    this.context.debug(
+      `Deploying websockets api with name ${config.name} to the ${config.region} region.`
+    )
 
     config.id = await getApiId({ apig2, id: config.id || this.state.id }) // validate with provider
 
@@ -53,15 +59,23 @@ class WebSockets extends Component {
     const providerRoutes = await getRoutes({ apig2, id: config.id })
 
     if (!config.id) {
-      this.ui.status(`Creating`)
+      this.context.status(`Creating`)
+      this.context.debug(
+        `Creating websockets api named ${config.name} in the ${config.region} region.`
+      )
       config.id = await createApi({ apig2, ...config })
     } else {
-      this.ui.status(`Updating`)
+      this.context.status(`Updating`)
+      this.context.debug(
+        `Updating websockets api named ${config.name} in the ${config.region} region.`
+      )
       await updateApi({ apig2, ...config })
     }
 
     const routesToDeploy = definedRoutes.filter((route) => not(providerRoutes.includes(route)))
     const routesToRemove = providerRoutes.filter((route) => not(definedRoutes.includes(route)))
+
+    this.context.debug(`Deploying routes for websockets api named ${config.name}.`)
 
     await Promise.all(
       routesToDeploy.map(async (route) => {
@@ -71,58 +85,62 @@ class WebSockets extends Component {
       })
     )
 
+    this.context.debug(`Removing outdated routes for websockets api named ${config.name}.`)
     // remove routes that don't exist in inputs
     await removeRoutes({ apig2, id: config.id, routes: routesToRemove })
 
+    this.context.debug(`Creating new deployment for websockets api named ${config.name}.`)
     // deploy the API
-    await createDeployment({ apig2, id: config.id, deploymentStage: this.context.stage })
+    await createDeployment({ apig2, id: config.id, deploymentStage: config.stage })
 
     config.url = getWebsocketUrl({
       id: config.id,
       region: config.region,
-      deploymentStage: this.context.stage
+      deploymentStage: config.stage
     })
 
-    // if the user has changed the id,
+    this.context.debug(
+      `Websockets api with id ${config.id} was successfully deployed in the ${config.region} region.`
+    )
+    this.context.debug(`Websockets url is ${config.url}.`)
+
+    // if the user has changed thse id,
     // remove the previous API
     if (this.state.id && this.state.id !== config.id) {
-      this.ui.status(`Replacing`)
+      this.context.status(`Replacing`)
       await removeApi({ apig2, id: config.id })
     }
 
+    this.state.name = config.name
     this.state.id = config.id
     this.state.url = config.url
+    this.state.region = config.region
     await this.save()
 
-    const outputs = pick(outputMask, config)
-
-    let routesOutputValue = `\n`
-    for (const route of Object.keys(outputs.routes)) {
-      routesOutputValue = `${routesOutputValue}    - ${route}\n`
-    }
-
-    this.ui.log()
-    this.ui.output('name', `  ${outputs.name}`)
-    this.ui.output('id', `    ${outputs.id}`)
-    this.ui.output('url', `   ${outputs.url}`)
-    this.ui.output('routes', `${routesOutputValue}`)
+    const outputs = pick(outputsList, config)
 
     return outputs
   }
 
-  async remove(inputs = {}) {
-    const config = { ...defaults, ...inputs }
-    config.id = config.id || this.state.id
+  async remove() {
+    const { id, region } = this.state
+
+    if (!id) {
+      this.context.debug(`Aboring removal. Websockets id not found in state.`)
+    }
     const apig2 = new aws.ApiGatewayV2({
-      region: config.region,
+      region: region,
       credentials: this.context.credentials.aws
     })
 
-    this.ui.status(`Removing`)
+    this.context.status(`Removing`)
+    this.context.debug(`Removing websockets api with id ${id} from the ${region} region.`)
 
-    if (config.id) {
-      await removeApi({ apig2, id: config.id })
-    }
+    await removeApi({ apig2, id })
+
+    this.context.debug(
+      `Websockets api with id ${id} was successfully removed from the ${region} region.`
+    )
 
     this.state = {}
     await this.save()
